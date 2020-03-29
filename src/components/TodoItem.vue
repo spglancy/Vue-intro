97de0f62c222b32137e4bf012af6d11f0f3a5cd5
Sean Glancy <spglancy@gmail.com>
2020-03-28 18:52:09 -0700

    created basic todo app

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: rgb(241, 241, 241);
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: red;
+  color: white;
+  border-radius: 100%;
+}
+</style>
